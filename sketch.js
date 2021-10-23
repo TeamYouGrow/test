ddc636cc90bedaccf6981f94f4dcdb7e540d5b91
TeamYouGrow <87800161+TeamYouGrow@users.noreply.github.com>
2021-10-23 20:03:04 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,14 +9,14 @@ Webcam Image Classification using a pre-trained customized model and p5.js
 This example uses p5 preload function to create the classifier
 === */
 
-/*
+
      var elems= null;
      document.addEventListener('DOMContentLoaded', function() {
     elems = document.querySelector('#modal1');
     var instances = M.Modal.init(elems);
     
          console.log(elems);
-          }); */
+          }); 
    
  //var instance = M.Modal.getInstance(elems);
 // Classifier Variable
