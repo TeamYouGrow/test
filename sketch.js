5fa1ef1f0905d1b30836a9fc2190eb0292daa722
TeamYouGrow <87800161+TeamYouGrow@users.noreply.github.com>
2021-10-26 18:19:35 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -109,8 +109,6 @@ else if(label === "Basil") {
 else if(label === "Aloe Vera") {
   //window.location.href ="video.html";
        //alert("Hello");
-     modalTitle = "Aloe Vera Tips";
-     modalContent =  "Click the video above to get tips on how to grow your Aloe Vera plant, straight from an expert.";
      instance.open();
  //
 }
